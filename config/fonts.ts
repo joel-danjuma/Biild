e6551dcdbc8e8f0f46292414f4031015ae4f8c11
joel danjuma <leojjad@gmail.com>
2023-10-17 09:06:55 +0100

    Updated Files

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,11 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
+import { Fira_Sans as FontMono, Inter as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const fontMono = FontMono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+  subsets: ["cyrillic"],
+  weight: ["100", "200", "300", "400", "500"],
+});
